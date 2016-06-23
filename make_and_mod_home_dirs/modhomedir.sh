8fc76eeca29a0694a98dea56653cc2ead81a0153
JNL <jnlickey@users.noreply.github.com>
2016-06-23 09:59:14 -0400

    Update modhomedir.sh

--- make_and_mod_home_dirs/modhomedir.sh
+++ make_and_mod_home_dirs/modhomedir.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # This script sets user home directory permissions
 # when it is first created.
+#
+# Created by: nlickey
+# 05/28/2015
 if [ -e /export/$2/$1 ]; then
         chown -R $1:cse_users /export/$2/$1
         chmod 711 /export/$2/$1
