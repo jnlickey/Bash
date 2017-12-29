e1d8def72bf00e2650b52913629fa3e2cb451f5b
JNL <jnlickey@users.noreply.github.com>
2017-12-28 22:25:15 -0500

    Add files via upload

---
+++ Control_brightness/brightness.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ $1 = "" || $1 = "-h" ]];then
+	echo "Usage: $0 <0-12>"
+	exit
+fi
+
+function build_expect () {
+	num=$1
+	echo "#!/usr/bin/expect" > /home/$USER/su_root.exp
+	echo "set num [lindex \$argv 0]" >> /home/$USER/su_root.exp
+	echo "spawn -noecho /bin/su  - root" >> /home/$USER/su_root.exp
+	echo "send \"echo \$num > /sys/class/backlight/acpi_video0/brightness\r\";" >> /home/$USER/su_root.exp
+	echo "log_user 0  # Disables output from send command" >> /home/$USER/su_root.exp
+	echo "send \"logout\r\"" >> /home/$USER/su_root.exp
+	echo "interact" >> /home/$USER/su_root.exp
+	chmod 770 /home/$USER/su_root.exp
+	sudo /home/$USER/su_root.exp $num > /dev/null
+}
+if [[ -f /home/$USER/su_root.exp ]];then
+	sudo ./su_root.exp $1 > /dev/null
+else
+	build_expect $1
+fi
+exit 0
---
+++ CopyProfile/CopyProfile.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+#
+# This script will copy your .bashrc profile to remote servers
+#
+# Created by: J.Lickey
+#
+if [[ $1 = "" || $1 = "-h" ]];then
+	printf "*** NOTE: Make sure that your .bashrc is setup on <ansible_server>. ***\n This script will copy <ansible_server>:/home/<e-id|c-id>/.bashrc to\nremote_server:/home/<e-id|c-id>/.bashrc\n"
+	echo ""
+	echo "Usage: $0 <e-idt|c-idt> -s [default:All|<servername>,<servername>]"
+	echo ""
+	exit
+fi
+function buildServerList () {
+	echo "[myservers]"  > /home/$id/ServerList.txt
+	echo $servers | sed 's/\,/\n/g'  >> /home/$id/ServerList.txt	
+}
+function buildYAML () {
+	echo "#"  > /home/$id/CopyProfile.yml
+	echo "# Auto-generated -- Copy profile YAML file"  >> /home/$id/CopyProfile.yml
+	echo "#" >> /home/$id/CopyProfile.yml
+	echo "# Created by: J.Lickey" >> /home/$id/CopyProfile.yml
+	echo "#" >> /home/$id/CopyProfile.yml
+	echo "---" >> /home/$id/CopyProfile.yml
+	echo "- hosts: all" >> /home/$id/CopyProfile.yml
+	echo "  become: yes" >> /home/$id/CopyProfile.yml
+	echo "" >> /home/$id/CopyProfile.yml
+	echo "  tasks:" >> /home/$id/CopyProfile.yml
+	echo "  - name: Copy Profile" >> /home/$id/CopyProfile.yml
+	echo "    copy:" >> /home/$id/CopyProfile.yml
+	echo "       src=\"{{path}}\"" >> /home/$id/CopyProfile.yml
+	echo "       dest=\"{{path}}\"" >> /home/$id/CopyProfile.yml
+	echo "       owner=\"{{id}}\"" >> /home/$id/CopyProfile.yml
+	echo "       group=\"{{id}}\"" >> /home/$id/CopyProfile.yml
+	echo "       mode=0640" >> /home/$id/CopyProfile.yml
+	echo "       backup=yes" >> /home/$id/CopyProfile.yml
+}
+function runAnsible () {
+	server=`echo ${server}| sed 's/\,/\|/g'`
+        echo "[myserverlist]" > /home/$id/serverlist
+	echo $server | sed 's/\|/\n/g' >> /home/$id/serverlist
+	if [[ ${server} =~ (all)|(ALL)|(All) ]];then
+		echo "Running the following command:"
+		echo "ansible -b -m copy -a 'src=/home/$id/.bashrc dest=/home/$id/.bashrc owner=$id group=$id mode=0640' all"
+		ansible-playbook -l /home/$id/serverlist -e "id=${id} path=/home/${id}/.bashrc" CopyProfile.yml
+		exit
+	elif [[ ${server} =~ "|" ]];then
+		echo "Running the following command:"
+                echo "ansible -b -m copy -a 'src=/home/${id}/.bashrc dest=/home/${id}/.bashrc owner=${id} group=${id} mode=0640' ~'${servers}'"
+		ansible-playbook -i /home/$id/ServerList.txt -e "id=${id} path=/home/${id}/.bashrc" CopyProfile.yml
+		exit
+	else
+		echo "Running the following command:" 
+                echo "ansible -b -m copy -e 'id=${id} server=${server}' -a 'src=/home/id/.bashrc dest=/home/${id}/.bashrc owner=${id} group=${id} mode=0640' ${server}"
+		ansible-playbook -l ${server} -e "id=${id} path=/home/${id}/.bashrc" CopyProfile.yml
+		exit
+	fi
+}
+function checkNumServers () {
+	num=`echo ${servers} | sed 's/,/\n/g' | wc -l`
+	if [[ $num = "1" ]];then
+		server=${servers}
+		runAnsible ${server}
+		exit
+	else
+		server=${servers}
+		checkServers ${server}
+	fi	
+}
+function checkServers (){
+	if [[ $server =~ (All)|(all)|(All) ]];then
+		echo "checkServers..."
+		server="${s/$server/all/p}"
+		runAnsible $server
+	elif [[ $server = "" ]];then
+		echo 'You forgot to add "-s all" or "-s <server>,<server>,..."'
+		exit
+	else 
+		servers=`echo $server | sed 's/\,/\|/g'`
+		runAnsible ${servers}
+                exit
+	fi
+	
+}
+function checkforid () {
+	if [[ $1 =~ ^e|^c ]];then
+		printf ""
+		id=$1
+	else
+		echo "Forgot to enter a valid e-id or c-id!"
+		exit
+	fi
+}
+servers=$3
+checkforid $1
+buildServerList $servers $id
+if [[ -e "/home/$id/CopyProfile.yml" ]];then
+	printf ""
+else
+	buildYAML $id
+fi
+if [[ $2 = "-s" ]];then
+	checkNumServers ${servers}
+	exit
+else
+	runAnsible ${servers}
+fi
+exit 0
